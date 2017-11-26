e42e823f6a8a9fdbb342a1589a9210001a8b8d7a
Arnav Garg <arnavga@gmail.com>
2017-11-25 18:04:24 -0800

    finished filter

--- homo.cpp
+++ homo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 
 #include <opencv2/core.hpp>
@@ -6,8 +7,10 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
-int main(int argc, char* argv[])
-{
+void fft(cv::Mat &src, cv::Mat &dst);
+cv::Mat butterworth(cv::Mat &img, int d0, int n, int high, int low);
+
+int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cout << "usage: ./homo <image_path>" << std::endl;
         return -1;
@@ -17,33 +20,91 @@ int main(int argc, char* argv[])
         std::cout << "no image data found" << std::endl;
         return -1;
     }
-    cv::Mat img(src);
 
-    // ln
-    for (int y = 0; y < img.rows; y++)
-        for (int x = 0; x < img.cols; x++)
-            for (int c = 0; c < 3; c++)
-                img.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(log(img.at<cv::Vec3b>(y, x)[c] + 1));
+    cv::Mat img;
+    std::vector<cv::Mat> hlsimg;
+    if (src.channels() == 1) {
+        img = cv::Mat(src);
+    } else if (src.channels() == 3) {
+        cv::Mat tmphls;
+        cv::cvtColor(src, tmphls, cv::COLOR_BGR2HLS);
+        cv::split(tmphls, hlsimg);
+        img = hlsimg[0];
+    } else {
+        return -1;
+    }
 
-    // FFT
-    cv::dft(img, img);
+    // apply FFT
+    cv::Mat fftimg;
+    fft(img, fftimg);
 
-    // High-Pass Filter
-    cv::Laplacian(img, img, 0);
+    // apply Butterworth HPS
+    cv::Mat filter = butterworth(fftimg, 10, 4, 100, 30);
+    cv::Mat bimg;
+    cv::Mat bchannels[] = {cv::Mat_<float>(filter.size()), cv::Mat::zeros(filter.size(), CV_32F)};
+    cv::merge(bchannels, 2, bimg);
+    cv::mulSpectrums(fftimg, bimg, fftimg, 0);
 
-    // inverse FFT
-    cv::idft(img, img);
+    // apply inverse FFT
+    cv::Mat ifftimg;
+    idft(fftimg, ifftimg, CV_HAL_DFT_REAL_OUTPUT);
 
-    // e^
-    for (int y = 0; y < img.rows; y++)
-        for (int x = 0; x < img.cols; x++)
-            for (int c = 0; c < 3; c++)
-                img.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(exp(img.at<cv::Vec3b>(y, x)[c]) + 1);
+    cv::Mat expimg;
+    cv::exp(ifftimg, expimg);
+    cv::imshow("test", expimg);
+    cv::Mat final;
+    if (src.channels() == 3)
+    {
+        hlsimg[0] = cv::Mat(expimg, cv::Rect(0, 0, hlsimg[1].cols, hlsimg[1].rows));
+        hlsimg[0].convertTo(hlsimg[0], CV_8U);
+        merge(&hlsimg[0], 3, img);
+        cv::cvtColor(img, final, cv::COLOR_HLS2BGR);
+    }
+    else if (src.channels() == 1)
+    {
+        final = expimg.clone();
+    }
 
     cv::namedWindow("original", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("filtered", cv::WINDOW_AUTOSIZE);
     cv::imshow("original", src);
-    cv::namedWindow("trans", cv::WINDOW_AUTOSIZE);
-    cv::imshow("trans", img);
-
+    cv::imshow("filtered", final);
     cv::waitKey(0);
 }
+
+void fft(cv::Mat &src, cv::Mat &dst)
+{
+    // convert to a 32F mat and take log
+    cv::Mat logimg;
+    src.convertTo(logimg, CV_32F);
+    cv::log(logimg+1, logimg);
+
+    // resize to optimal fft size
+    cv::Mat padded;
+    int m = cv::getOptimalDFTSize(src.rows);
+    int n = cv::getOptimalDFTSize(src.cols);
+    cv::copyMakeBorder(logimg, padded, 0, m-logimg.rows, 0, n-logimg.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+
+    // add imaginary column to mat and apply fft
+    cv::Mat plane[] = {cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F)};
+    cv::Mat imgComplex;
+    cv::merge(plane, 2, imgComplex);
+    cv::dft(imgComplex, dst);
+}
+
+cv::Mat butterworth(cv::Mat &img, int d0, int n, int high, int low) {
+    cv::Mat single(img.rows, img.cols, CV_32F);
+    int cx = img.rows / 2;
+    int cy = img.cols / 2;
+    float upper = high * 0.01;
+    float lower = low * 0.01;
+
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {
+            double radius = sqrt(pow(i - cx, 2) + pow(j - cy, 2));
+            single.at<float>(i, j) = ((upper - lower) * (1 / pow(d0 / radius, 2 * n))) + lower;
+        }
+    }
+    return single;
+}
+
